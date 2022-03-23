13eb535817d1c19ceda6cdd5315313de6e2c1081
mIhoom <mdmichalski.poczta@gmail.com>
2022-03-23 13:44:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Podaj dwie liczby celem ich podzielenia")
+try:
+    a = int(input("Podaj pierwszą liczbę: "))
+    b = int(input("Podaj drugą liczbę: "))
+    result = a / b
+    print("Wynik dzielenia to: ", result)
+except ValueError:
+    print("Podaj dane liczbowe!")
+except ZeroDivisionError:
+    result = "Pamiętaj cholero, nie dziel przez zero!"
+    print(result)
+
